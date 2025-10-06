6c757b2b3bc9aace70f17abe8bb97a1589b1dcc3
Denner <dennermpb@hotmail.com>
2025-10-06 16:17:13 -0300

    Fix frmaBorder iframe

--- src/Components/Content/Header.tsx
+++ src/Components/Content/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px32 text-white">
           <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20" >A melhor experiência de música ao vivo para seu evento!</h2>
           <div className="flex justify-center items-center mt-20">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/l9WYXsxv-Ug?si=oLByUUE22672nzrk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/l9WYXsxv-Ug?si=oLByUUE22672nzrk" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
           </div>
           <div className="space-x-6 mt-20 mb-20">
             <WhatsAppButton />
